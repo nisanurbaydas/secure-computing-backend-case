ac80e2271cd76f27cced00abe4fc6eb70bf91288
nisanurbaydas <n.nurb@hotmail.com>
2023-01-14 02:12:31 +0300

    swagger integration

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const config = require('./config/index');
 const loaders = require('./loaders');
 const errorHandler = require('./middlewares/errorHandler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const { TaskRoutes } = require('./routes');
 
@@ -12,6 +14,8 @@ config();
 loaders();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.APP_PORT;
 app.listen(PORT, () => {
