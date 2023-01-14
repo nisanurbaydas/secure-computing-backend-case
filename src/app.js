9720e2658dcf9575a1029ebf2601881a45ee2d3e
nisanurbaydas <n.nurb@hotmail.com>
2023-01-14 17:50:39 +0300

    jsdoc(swagger) implemented

--- src/app.js
+++ src/app.js
@@ -2,11 +2,34 @@ const express = require('express');
 
 const config = require('./config/index');
 const loaders = require('./loaders');
-const errorHandler = require('./middlewares/errorHandler');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
 
 const { TaskRoutes } = require('./routes');
+
+const errorHandler = require('./middlewares/errorHandler');
+
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+//const swaggerDocument = require('../swagger.json');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Todo App',
+      version: '1.0.0',
+      description: 'A simple Express Todo app with CRUD operations',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const specs = swaggerJSDoc(options);
 
 const app = express();
 
@@ -15,13 +38,14 @@ loaders();
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.APP_PORT;
 app.listen(PORT, () => {
   console.log(`App listening on port 3000`);
 
   app.use('/tasks', TaskRoutes);
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
   app.use((req, res, next) => {
     const error = new Error('Page you are looking for does not exist');
--- src/routes/Task.js
+++ src/routes/Task.js
@@ -1,14 +1,158 @@
 const express = require('express');
 
-const { index, create, fetchTask, update, deleteTask } = require('../controllers/Task');
+const { index, fetchTask, create, update, deleteTask } = require('../controllers/Task');
 const idChecker = require('../middlewares/idChecker');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Task:
+ *       type: object
+ *       required:
+ *          - name
+ *          - status
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The task's name.
+ *           example: Learn about NodeJS.
+ *         status:
+ *           type: string
+ *           description: The task's status.
+ *           example: backlog
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: The Todo app
+ */
+
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder tasks.
+ *     description: Retrieve a list of tasks from JSONPlaceholder.
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: A list of tasks.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                $ref: '#/components/schemas/Task'
+ */
 router.get('/', index);
-router.post('/', create);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   get:
+ *     summary: Get the task by id
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *     responses:
+ *       200:
+ *         description: The task details by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       404:
+ *         description: The Task was not found
+ */
 router.route('/:id').get(idChecker(), fetchTask);
+
+/**
+ * @swagger
+ * /tasks:
+ *   post:
+ *     summary: Create a new task
+ *     tags: [Tasks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       201:
+ *         description: The task was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       500:
+ *         description: Some server error
+ */
+router.post('/', create);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *  put:
+ *    summary: Update the task by the id
+ *    tags: [Tasks]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The task id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Task'
+ *    responses:
+ *      200:
+ *        description: The task was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Task'
+ *      404:
+ *        description: The task was not found
+ *      500:
+ *        description: Some error happened
+ */
 router.route('/:id').put(idChecker(), update);
+
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   delete:
+ *     summary: Remove the task by id
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task id
+ *
+ *     responses:
+ *       200:
+ *         description: The task was deleted
+ *       404:
+ *         description: The task was not found
+ */
 router.route('/:id').delete(idChecker(), deleteTask);
 
 module.exports = router;
